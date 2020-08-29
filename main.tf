23254fed18d8072165f2c94e091e4b86dc5cef69
Scott Cressi <scottcressi@gmail.com>
2020-08-29 14:20:06 -0400

    split

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  version = "~> 3.0"
-  region  = "us-east-1"
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "my-vpc"
@@ -12,22 +7,6 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = true
   enable_vpn_gateway = true
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.my-cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.my-cluster.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
 }
 
 module "my-cluster" {
