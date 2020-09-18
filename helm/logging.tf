2b6d68b8e903a826291e208117e30d2d6133ffed
Scott Cressi <scottcressi@gmail.com>
2020-09-18 10:04:54 -0400

    shared, metricbeat

--- helm/logging.tf
+++ helm/logging.tf
@@ -12,6 +12,48 @@ resource "kubernetes_namespace" "logging" {
   }
 }
 
+resource "helm_release" "metricbeat" {
+  name       = "metricbeat"
+  repository = "https://helm.elastic.co"
+  chart      = "metricbeat"
+  version    = "7.9.1"
+  namespace  = "logging"
+
+  depends_on = [kubernetes_namespace.logging]
+
+  values = [<<EOF
+daemonset:
+  metricbeatConfig:
+    metricbeat.yml: |
+      metricbeat.modules:
+      - module: prometheus
+        period: 10s
+        metricsets: ["collector"]
+        hosts: ["prometheus-operator-prometheus.monitoring:9090"]
+        metrics_path: /metrics
+      output.logstash:
+        hosts: 'logstash-logstash.logging:5044'
+deployment:
+  metricbeatConfig:
+    metricbeat.yml: |
+      metricbeat.modules:
+      - module: kubernetes
+        enabled: true
+        metricsets:
+          - state_node
+          - state_deployment
+          - state_replicaset
+          - state_pod
+          - state_container
+        period: 10s
+        hosts: ["prometheus-operator-kube-state-metrics.monitoring:8080"]
+      output.logstash:
+        hosts: 'logstash-logstash.logging:5044'
+EOF
+  ]
+
+}
+
 resource "helm_release" "opendistro" {
   name       = "opendistro"
   chart      = "/var/tmp/opendistro-build/helm/opendistro-es/"
