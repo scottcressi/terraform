33485418bfe9d512476ea175ea5015811d3f1ba4
Scott Cressi <scottcressi@gmail.com>
2020-09-21 18:39:29 -0400

    doc

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,11 +1,14 @@
 variable "location" {
+  description = "choices: local | aws"
   type        = string
 }
 
 variable "environment" {
+  description = "choices: local | dev | shared | $ENV"
   type        = string
 }
 
 variable "zone" {
+  description = "ex. 'example' if you own example.com"
   type        = string
 }
