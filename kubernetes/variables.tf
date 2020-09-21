272445e17ec5c71a2c171ad4c892c186e6bf2289
Scott Cressi <scottcressi@gmail.com>
2020-09-21 18:17:55 -0400

    location, prompt for vars

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,21 +1,11 @@
 variable "location" {
   type        = string
-  default     = "local"
 }
 
 variable "environment" {
   type        = string
-  default     = "local"
 }
 
 variable "zone" {
   type        = string
-  default     = "example"
-}
-
-variable "nginx-ingress-certarn" {
-  type        = map
-  default = {
-      local = "foo"
-    }
 }
