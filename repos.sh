9a72db1c1c34ddf75f52217a34ab807536c441c2
Scott Cressi <scottcressi@gmail.com>
2020-09-22 15:09:40 -0400

    repos

---
+++ repos.sh
@@ -0,0 +1,3 @@
+helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.5.8/charts/
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add bitnami https://charts.bitnami.com/bitnami
