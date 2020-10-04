71463ace6d6a7a1f999a4603dc77e49a8a46d310
Scott Cressi <scottcressi@gmail.com>
2020-10-03 21:29:20 -0400

    versions

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -1,9 +1,15 @@
 provider "aws" {
   region = "us-east-1"
+  version = "3.9.0"
 }
 
 provider "vault" {
   address = "http://vault-k8s.${var.environment}.${var.zone}.com:8200"
+  version = "2.14.0"
+}
+
+provider "helm" {
+  version = "1.3.1"
 }
 
 terraform {
