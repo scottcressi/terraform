2f802dfe398e1b3aaf4a6ede3681ac7ab9f77998
Scott Cressi <scottcressi@gmail.com>
2020-10-03 23:50:54 -0400

    var

--- secrets.sh
+++ secrets.sh
@@ -1,13 +1,13 @@
 #!/usr/local/env bash
 
+export VAULT_ADDR=http://localhost:8200
+export VAULT_TOKEN=root
+
 if ! pgrep vault > /dev/null ; then
-    vault server -dev -dev-root-token-id="root" & disown
+    vault server -dev -dev-root-token-id="$VAULT_TOKEN" & disown
     echo waiting for initialization
     sleep 1
 fi
-
-export VAULT_ADDR=http://localhost:8200
-export VAULT_TOKEN=root
 
 vault kv put secret/helm/kubewatch slack_token=foo ; sleep .5
 
