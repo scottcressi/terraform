5c5d9f713bfbe1ed34a21e1d127628ba87048e33
Scott Cressi <scottcressi@gmail.com>
2020-10-13 10:56:08 -0400

    lock more providers

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -22,3 +22,19 @@ provider "kubernetes" {
   token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
   version                = "1.13.2"
 }
+
+provider "random" {
+  version = "3.0.0"
+}
+
+provider "local" {
+  version = "1.4.0"
+}
+
+provider "null" {
+  version = "3.0.0"
+}
+
+provider "template" {
+  version = "2.2.0"
+}
