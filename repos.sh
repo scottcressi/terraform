41a343f9dfeaadbfc9db0c5e4293a7c5b01642ed
Scott Cressi <scottcressi@gmail.com>
2020-10-13 12:36:50 -0400

    deprecate

--- repos.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/env bash
-
-helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.5.8/charts/
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo add bitnami https://charts.bitnami.com/bitnami
