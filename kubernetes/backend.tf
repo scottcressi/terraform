44b78811546b4d8238af0cdcf5a8468602602f73
Scott Cressi <scottcressi@gmail.com>
2020-11-20 11:33:24 -0500

    leave in commented backend

--- kubernetes/backend.tf
+++ kubernetes/backend.tf
@@ -1,4 +1,13 @@
 terraform {
   required_version = "0.13.5"
+
   backend "pg" {}
+
+  #backend "s3" {
+  #  bucket         = "374012539393-terraform-state"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #  #dynamodb_table = "terraform_state"
+  #}
+
 }
