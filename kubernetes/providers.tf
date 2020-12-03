f2af0d23aaf2a1c4bd9091fe9cf50909857347f1
Scott Cressi <scottcressi@gmail.com>
2020-12-03 10:04:18 -0500

    version

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -16,7 +16,7 @@ provider "kubernetes" {
   host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
   cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
   token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
-  version                = "1.13.2"
+  version                = "1.13.3"
 }
 
 provider "random" {
