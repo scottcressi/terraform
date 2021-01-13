0e7104d5e198ef0964373009eeef8d2ec8ba50c6
Scott Cressi <scottcressi@gmail.com>
2021-01-12 22:10:37 -0500

    folding, no args, secrets in one line

--- local.sh
+++ local.sh
@@ -8,40 +8,40 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ $# -eq 0 ] ; then
     echo """
-options:
-init_vault
-init_postgres
-"""
+    options:
+    init_vault
+    init_postgres
+    """
     exit 0
 fi
 
 init_vault(){
-# create secrets
-docker-compose up -d vault
-sleep 2
-docker exec -ti terraform_vault_1 sh -c "export VAULT_TOKEN=root ; vault kv put -address http://127.0.0.1:8200 secret/helm/kubewatch slack_token=foo"
-sleep 2
-docker exec -ti terraform_vault_1 sh -c "export VAULT_TOKEN=root ; vault kv put -address http://127.0.0.1:8200 secret/helm/prometheus slack_token=foo"
-sleep 2
-echo """
-set the following in your /etc/hosts:
-127.0.0.1 vault-k8s.ENV.ZONE.com
-"""
+    # create secrets
+    docker-compose up -d vault
+    sleep 2
+    docker exec -ti terraform_vault_1 sh -c "export VAULT_TOKEN=root \
+    ; vault kv put -address http://127.0.0.1:8200 secret/helm/kubewatch slack_token=foo \
+    ; vault kv put -address http://127.0.0.1:8200 secret/helm/prometheus slack_token=foo \
+    "
+    echo """
+    set the following in your /etc/hosts:
+    127.0.0.1 vault-k8s.ENV.ZONE.com
+    """
 }
 
 init_postgres(){
-# create vault and postgres backends
-POSTGRES_ADDRESS=localhost
-POSTGRES_USER=terraform
-POSTGRES_PASSWORD=terraform
-export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-docker-compose up -d postgres
+    # create vault and postgres backends
+    POSTGRES_ADDRESS=localhost
+    POSTGRES_USER=terraform
+    POSTGRES_PASSWORD=terraform
+    export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+    docker-compose up -d postgres
 
-# terraform init backend
-cd "$DIR"/kubernetes || exit
-terraform init -backend-config="conn_str=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_ADDRESS/terraform_backend?sslmode=disable" || echo "Done !"
-terraform workspace new default || if [ $? -eq 0 ]; then echo "Done !"; else echo "Workspace \"default\" was not created"; fi
-terraform init
+    # terraform init backend
+    cd "$DIR"/kubernetes || exit
+    terraform init -backend-config="conn_str=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_ADDRESS/terraform_backend?sslmode=disable" || echo "Done !"
+    terraform workspace new default || if [ $? -eq 0 ]; then echo "Done !"; else echo "Workspace \"default\" was not created"; fi
+    terraform init
 }
 
 "$@"
