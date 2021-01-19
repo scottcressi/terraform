6ff9c3f42ccd0548ad46434f6c59f51c4420876f
Scott Cressi <scottcressi@gmail.com>
2021-01-19 15:54:42 -0500

    fix subnets for ingressing, azs

--- kubernetes/vpc.tf
+++ kubernetes/vpc.tf
@@ -4,18 +4,20 @@ module "vpc" {
   version            = "2.66.0"
   name               = "my-vpc"
   cidr               = "10.0.0.0/16"
-  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs                = data.aws_availability_zones.available.names
+  private_subnets    = ["10.0.1.0/24",   "10.0.2.0/24",   "10.0.3.0/24"]
   public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/k8s.org.com" = "owned"
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
   }
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/k8s.org.com" = "owned"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 
   customer_gateways = {
@@ -25,4 +27,7 @@ module "vpc" {
     }
   }
 
+}
+
+data "aws_availability_zones" "available" {
 }
