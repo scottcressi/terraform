14d7a2ad2a62efb3059e08270eecf0e2e34eb4b5
Scott Cressi <scottcressi@gmail.com>
2021-02-06 13:33:41 -0500

    split

--- kubernetes/aws/network/vpc.tf
+++ kubernetes/aws/network/vpc.tf
@@ -25,21 +25,3 @@ module "vpc" {
   }
 
 }
-
-data "aws_availability_zones" "available" {
-}
-
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.private_subnets
-}
