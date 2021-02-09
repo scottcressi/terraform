d0eff153c35cd9669fcfc6ff3b0cef732a439b1d
Scott Cressi <scottcressi@gmail.com>
2021-02-08 21:51:38 -0500

    breaks

--- kubernetes/aws/eks/providers.tf
+++ kubernetes/aws/eks/providers.tf
@@ -7,7 +7,6 @@ provider "vault" {
 }
 
 provider "kubernetes" {
-  config_path            = "kubeconfig_my-cluster"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
