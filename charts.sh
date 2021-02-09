c068cd48998c2f66d389cdc3710258f67c2bb331
Scott Cressi <scottcressi@gmail.com>
2021-02-08 22:54:23 -0500

    fix

--- charts.sh
+++ charts.sh
@@ -1,14 +1,14 @@
 #!/usr/local/env sh
 
 # open distro
-git clone https://github.com/opendistro-for-elasticsearch/opendistro-build.git /var/tmp/opendistro  -build
-cd /var/tmp/opendistro-build/helm/opendistro-es || exit
+git clone https://github.com/opendistro-for-elasticsearch/opendistro-build.git /var/tmp/opendistro
+cd /var/tmp/opendistro/helm/opendistro-es || exit
 git pull
-git checkout v1.11.0
+git checkout v1.12.0
 helm package .
 
 # docker-registry-ui
 git clone https://github.com/Joxit/docker-registry-ui.git /var/tmp/docker-registry-ui
 cd /var/tmp/docker-registry-ui || exit
 git pull
-git checkout 1.5.0
+git checkout 1.5.2
