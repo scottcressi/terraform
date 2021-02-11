85aa153814b530381c6af615a67dcdf16318f4ff
Scott Cressi <scottcressi@gmail.com>
2021-02-11 09:36:42 -0500

    remove jobs, plugin update, version

--- kubernetes/aws/helm/jenkins.tf
+++ kubernetes/aws/helm/jenkins.tf
@@ -32,7 +32,7 @@ controller:
     <authorizationStrategy class="hudson.security.ProjectMatrixAuthorizationStrategy">
       <permission>hudson.model.Hudson.Administer:admin</permission>
     </authorizationStrategy>
-  tag: "2.235.5"
+  tag: "2.263.4"
   csrf:
     defaultCrumbIssuer:
       enabled: true
@@ -46,71 +46,22 @@ controller:
       kubernetes.io/tls-acme: "true"
       nginx.ingress.kubernetes.io/whitelist-source-range: "0.0.0.0"
   installPlugins:
-  - kubernetes
-  - workflow-job
-  - workflow-aggregator
+  - ansicolor
+  - configuration-as-code
   - credentials-binding
   - git
-  - matrix-auth
-  - ansicolor
-  - prometheus
+  - github
   - job-dsl
+  - kubernetes
+  - matrix-auth
+  - pipeline-aws
+  - prometheus
+  - slack
+  - workflow-aggregator
+  - workflow-job
   overwriteJobs: true
   overwriteConfig: true
   numExecutors: 1
-  jobs:
-    seed: |-
-      <?xml version='1.1' encoding='UTF-8'?>
-      <project>
-        <description></description>
-        <keepDependencies>false</keepDependencies>
-        <properties/>
-        <scm class="hudson.plugins.git.GitSCM" plugin="git@4.3.0">
-          <configVersion>2</configVersion>
-          <userRemoteConfigs>
-            <hudson.plugins.git.UserRemoteConfig>
-              <url>https://github.com/${var.jenkinsgithubuser}/jenkins.git</url>
-            </hudson.plugins.git.UserRemoteConfig>
-          </userRemoteConfigs>
-          <branches>
-            <hudson.plugins.git.BranchSpec>
-              <name>*/master</name>
-            </hudson.plugins.git.BranchSpec>
-          </branches>
-          <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
-          <submoduleCfg class="list"/>
-          <extensions/>
-        </scm>
-        <canRoam>true</canRoam>
-        <disabled>false</disabled>
-        <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
-        <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
-        <triggers>
-          <hudson.triggers.SCMTrigger>
-            <spec>* * * * *</spec>
-            <ignorePostCommitHooks>false</ignorePostCommitHooks>
-          </hudson.triggers.SCMTrigger>
-        </triggers>
-        <concurrentBuild>false</concurrentBuild>
-        <builders>
-          <javaposse.jobdsl.plugin.ExecuteDslScripts plugin="job-dsl@1.77">
-            <targets>seed.groovy</targets>
-            <usingScriptText>false</usingScriptText>
-            <sandbox>false</sandbox>
-            <ignoreExisting>false</ignoreExisting>
-            <ignoreMissingFiles>false</ignoreMissingFiles>
-            <failOnMissingPlugin>false</failOnMissingPlugin>
-            <failOnSeedCollision>false</failOnSeedCollision>
-            <unstableOnDeprecation>false</unstableOnDeprecation>
-            <removedJobAction>DELETE</removedJobAction>
-            <removedViewAction>DELETE</removedViewAction>
-            <removedConfigFilesAction>DELETE</removedConfigFilesAction>
-            <lookupStrategy>JENKINS_ROOT</lookupStrategy>
-          </javaposse.jobdsl.plugin.ExecuteDslScripts>
-        </builders>
-        <publishers/>
-        <buildWrappers/>
-      </project>
 EOF
   ]
 
