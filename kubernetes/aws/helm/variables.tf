32760778b12a1c91367952da315200742221ca3b
Scott Cressi <scottcressi@gmail.com>
2021-02-22 14:16:36 -0500

    remove defaults

--- kubernetes/aws/helm/variables.tf
+++ kubernetes/aws/helm/variables.tf
@@ -6,23 +6,19 @@ variable "environment" {
   dev/etc - a non shared real environment cluster
   EOT
   type        = string
-  default = "dev"
 }
 
 variable "zone" {
   description = "ex. 'example' if you own example.com"
   type        = string
-  default = "scottcressi"
 }
 
 variable "region" {
   description = "ex. us-east-1"
   type        = string
-  default = "us-east-1"
 }
 
 variable "jenkinsgithubuser" {
   description = "ex. example"
   type        = string
-  default = "scottcressi"
 }
