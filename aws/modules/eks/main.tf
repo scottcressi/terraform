113bfa8aa130912eeac0601da0242dee70459772
Scott Cressi <scottcressi@gmail.com>
2021-02-26 09:33:51 -0500

    tagged groups for node selectors

--- aws/modules/eks/main.tf
+++ aws/modules/eks/main.tf
@@ -7,13 +7,35 @@ module "my-cluster" {
   subnets         = data.terraform_remote_state.network.outputs.private_subnets
   vpc_id          = data.terraform_remote_state.network.outputs.vpc_id
 
-  worker_groups = [
-    {
-      instance_type        = "m5.large"
-      asg_min_size         = 4
-      asg_max_size         = 6
-      asg_desired_capacity = 5
+  node_groups = {
+    example = {
+      desired_capacity = 3
+      max_capacity     = 4
+      min_capacity     = 1
 
+      instance_types = ["m5.large"]
+      capacity_type  = "SPOT"
+      k8s_labels = {
+        group = "test"
+      }
+      additional_tags = {
+        ExtraTag = "example"
+      }
     }
-  ]
+    example1 = {
+      desired_capacity = 3
+      max_capacity     = 4
+      min_capacity     = 1
+
+      instance_types = ["m5.large"]
+      capacity_type  = "SPOT"
+      k8s_labels = {
+        group = "test1"
+      }
+      additional_tags = {
+        ExtraTag = "example1"
+      }
+    }
+  }
+
 }
