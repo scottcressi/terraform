b1baad9574c84e4f8fb8bdaf6de1b5aa1d95e21c
Scott Cressi <scottcressi@gmail.com>
2021-03-03 09:32:38 -0500

    puppet

--- aws/modules/ec2/locals.tf
+++ aws/modules/ec2/locals.tf
@@ -3,5 +3,7 @@ locals {
 #!/bin/bash
 echo foo > /tmp/foo.txt
 sudo yum install -y telnet
+sudo yum install -y https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
+sudo yum install -y puppet-agent
 EOF
 }
