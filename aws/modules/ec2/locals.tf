b971c2f5dafc7732ebacaf6b9bb89a6d11948ad0
Scott Cressi <scottcressi@gmail.com>
2021-03-03 09:34:48 -0500

    ncat

--- aws/modules/ec2/locals.tf
+++ aws/modules/ec2/locals.tf
@@ -2,7 +2,7 @@ locals {
   instance-userdata = <<EOF
 #!/bin/bash
 echo foo > /tmp/foo.txt
-sudo yum install -y telnet
+sudo yum install -y nmap-ncat
 sudo yum install -y https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
 sudo yum install -y puppet-agent
 EOF
