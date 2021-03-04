19067f89a2697d5aa8ab3a74421176fb67bd80c8
Scott Cressi <scottcressi@gmail.com>
2021-03-04 08:25:45 -0500

    convert to file for non destructive updates

--- aws/environments/dev/us-east-1/ec2/user_data.sh
+++ aws/environments/dev/us-east-1/ec2/user_data.sh
@@ -1,9 +1,5 @@
-locals {
-  instance-userdata = <<EOF
 #!/bin/bash
 echo foo > /tmp/foo.txt
 sudo yum install -y nmap-ncat
 sudo yum install -y https://yum.puppet.com/puppet6/puppet6-release-el-7.noarch.rpm
 sudo yum install -y puppet-agent
-EOF
-}
