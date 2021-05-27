4a4d564265ef1c2b378c51b99637152f65faaa11
Scott Cressi <scottcressi@gmail.com>
2021-05-27 08:06:19 -0400

    no puppet

--- aws/environments/dev/us-east-1/ec2/user_data.sh
+++ aws/environments/dev/us-east-1/ec2/user_data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo foo > /tmp/foo.txt
 sudo yum install -y nmap-ncat
-sudo yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
-sudo yum install -y puppet-agent
