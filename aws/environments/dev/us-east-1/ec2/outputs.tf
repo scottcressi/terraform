cdcfcfbd4f20b43908145cd13eecf636163d98f2
Scott Cressi <scottcressi@gmail.com>
2021-06-08 22:08:08 -0400

    ami user

--- aws/environments/dev/us-east-1/ec2/outputs.tf
+++ aws/environments/dev/us-east-1/ec2/outputs.tf
@@ -1,4 +1,7 @@
 output "public_ip" {
-  description = "List of public IP addresses assigned to the instances, if applicable"
   value       = module.ec2.public_ip
+}
+
+output "ami_user" {
+  value       = "ubuntu"
 }
