d07a9ffab0594cdc369eaa0a061dc8559add8f80
Scott Cressi <scottcressi@gmail.com>
2021-06-09 20:33:09 -0400

    update

--- aws/modules/helm/argocd.tf
+++ aws/modules/helm/argocd.tf
@@ -1,26 +1,23 @@
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    annotations = {
-      name = "example-annotation"
-    }
-
-    labels = {
-      mylabel = "label-value"
-    }
-
-    name = "argocd"
-  }
-}
-
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "1.8.4"
-  namespace  = "default"
-
-  depends_on = [kubernetes_namespace.argocd]
+  version    = "3.6.8"
+  namespace  = "argocd"
+  create_namespace = true
   values = [<<EOF
+server:
+  config:
+    application.instanceLabelKey: argocd.argoproj.io/instance
+    repositories: |
+      - sshPrivateKeySecret:
+          key: sshPrivateKey
+          name: github
+        url: git@github.com:scottcressi/helm
+
+controller:
+  args:
+    appResyncPeriod: "60"
 EOF
   ]
 
