0c52f49557a05197b68b289dc8da5a80e233e854
Scott Cressi <scottcressi@gmail.com>
2021-06-11 00:31:34 -0400

    deprecate

--- providers/aws/environments/dev/us-east-1/helm/main.tf
+++
@@ -1,8 +0,0 @@
-module "helm" {
-  source      = "../../../../../../modules/helm"
-  environment = var.environment
-  region      = var.region
-  zone        = var.zone
-  state_key    = var.state_key
-  state_bucket = var.state_bucket
-}
