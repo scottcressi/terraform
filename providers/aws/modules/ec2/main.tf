0ed479b7876cbf5ad420d13c9de549f0d27d5aa9
Scott Cressi <scottcressi@gmail.com>
2021-06-30 10:30:05 -0400

    recovery window

--- providers/aws/modules/ec2/main.tf
+++ providers/aws/modules/ec2/main.tf
@@ -33,6 +33,7 @@ resource "aws_secretsmanager_secret_version" "example" {
 }
 
 resource "aws_secretsmanager_secret" "some_ssh_key" {
+  recovery_window_in_days = 0
   name = "secret/ssh_keys/some_ssh_key"
 }
 
